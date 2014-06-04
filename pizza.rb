350133d5a6f82cfea588930752f0a3ff87eb25a1
Joseph Tingsanchali <charnwoot@gmail.com>
2014-06-04 09:36:58 -0500

    Add a failing test for Topping.initialize.

--- pizza.rb
+++ pizza.rb
@@ -1,5 +1,22 @@
 class Pizza
+  attr_accessor :toppings
+
+  def initialize(toppings)
+  	@toppings = toppings
+  end
 end
 
 class Topping
+  attr_accessor :name, :vegetarian
+  # Instantiate a new topping.
+  #
+  # name 	   - The String name of the topping.
+  # vegetarian - The Boolean indicating whether or not the topping is
+  #              vegetarian.
+  #
+  # Returns a new Topping object.
+  def initialize(name, vegetarian: false)
+    @name = name
+    @vegetarian = vegetarian
+  end
 end
