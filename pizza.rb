14ef8ea4c70d11093a26d369d81e477bc21825a8
Joseph Tingsanchali <charnwoot@gmail.com>
2014-06-04 16:07:15 -0500

    Fixed delivery and late time methods and tests for Pizza class

--- pizza.rb
+++ pizza.rb
@@ -1,8 +1,24 @@
 class Pizza
-  attr_accessor :toppings
+  attr_accessor :toppings, :delivery_time
 
   def initialize(toppings = [Topping.new('cheese')])
   	@toppings = toppings
+  end
+  def vegetarian?
+  	@toppings.all? { |top| top.vegetarian }
+  end
+  def add_topping(new_topping)
+  	@toppings << new_topping
+  end
+  def deliver!
+		@delivery_time = Time.now.to_i + 30*60
+  end
+  def late?
+  	if @delivery_time.to_i > Time.now.to_i
+  		return true
+  	else
+  		return false
+  	end
   end
 end
 
--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -9,9 +9,9 @@ describe Pizza do
   	it 'defaults the toppings to cheese only, if the pizza has no toppings' do
   		pizza = Pizza.new
 
-		expect(pizza.toppings.size).to eq(1)
-  	expect(pizza.toppings.first.name).to eq('cheese')
-	end
+			expect(pizza.toppings.size).to eq(1)
+  		expect(pizza.toppings.first.name).to eq('cheese')
+	  end
   	it 'records all of the toppings' do
   		toppings = [
   			Topping.new('mushrooms', vegetarian: true),
@@ -19,9 +19,42 @@ describe Pizza do
   		]
   		pizza = Pizza.new(toppings)
 
-  		expect(pizza.toppings).to eq(toppings)
+  		expect(pizza.toppings).to eq(toppings)	
   	end
   end
+  describe '.vegetarian?' do
+    it 'confirm toppings are vegetarian' do
+  	  toppings = [Topping.new('tomato',vegetarian: true)]
+  	  pizza = Pizza.new(toppings)
+ 		  
+      expect(pizza.vegetarian?).to eq(true)
+ 	  end
+  end
+  describe '.add_topping' do
+ 	  it 'adds toppings to total array of toppings' do
+ 		  toppings = [Topping.new('tomato',vegetarian: true)]
+ 		  pizza = Pizza.new(toppings)
+ 		  pizza.add_topping(Topping.new('sausage',vegetarian:false))
+      expect(pizza.toppings.size).to eq(2)
+    end  
+  end
+  describe '.deliver!' do
+    # let(:now) { Time.now }
+    # let(:now) { Timecop.freeze(Time.now) }
+    it 'schedules a pizza to be delivered 30 minutes in the future' do
+      pizza = Pizza.new
+      result = pizza.deliver!
+      # converted time to integer (seconds precision), may fail when 23:59:59.999999999
+      # or however specified latest possible Time
+      expect(pizza.delivery_time).to eq(Time.now.to_i + 30*60)
+    end
+  end
+  describe '.late?' do
+    it 'confirm if delivery time is late' do
+      pizza = Pizza.new
+    end
+  end
+
 end
 
 describe Topping do
@@ -41,4 +74,8 @@ describe Topping do
   	end
   end
 end
+
+
+
+
 
